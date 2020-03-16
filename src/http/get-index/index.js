36a318f9be2674878ffdd7a70a0bc49c4b55202d
Paul Chin Jr <pchinjr@gmail.com>
2020-03-16 00:22:23 -0400

    removed comment

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-//const tiny = require('tiny-json-http')
 
 exports.handler = async function http(req) {
   const data = await axios.get("https://covid19js.com/dist/covid19.json")
