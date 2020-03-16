9bf19d0bced7eb58d4f7b6fe165e3c4d82e4ccbd
Paul Chin Jr <pchinjr@gmail.com>
2020-03-16 14:58:49 -0400

    KJ's trycatch

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,12 +1,23 @@
 const axios = require('axios');
 
 exports.handler = async function http(req) {
-  const data = await axios.get("https://covid19js.com/dist/covid19.json")
-  let result = JSON.stringify(data.data)
-  
-  return {
-    headers: { "content-type": "application/json; charset=utf8" },
-    body: result
-  };
-};
+  let result
+  try {
+    const response = await axios.get("https://covid19js.com/dist/covid19.json")
+    // console.log('Response: ', response) // Looks like: { data: [...] } // An Object
+    let data = response.data
+    // console.log('Data: ', data) // Looks like: [...] // An Array
+    result = JSON.stringify(data)
+    // to return JSON we need to encode the Array as JSON
+  } catch(err) {
+    // If there is an error fetching or encoding we can catch it here
+    console.error(err)
+    result = err
+  }
 
+  return {
+    headers: { "content-type": "application/json; charset=UTF-8" },
+    // If we want to return JSON as stated 👆 then we need to make sure it is encoded as JSON
+    body: result
+  }
+}
